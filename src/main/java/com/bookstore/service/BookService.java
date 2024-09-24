02871d7f0f8b63c5784fea4501bf79f91b780310
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-09-24 10:37:50 +0200

    added pagination and swagger

--- src/main/java/com/bookstore/service/BookService.java
+++ src/main/java/com/bookstore/service/BookService.java
@@ -3,13 +3,14 @@ package com.bookstore.service;
 import com.bookstore.dto.BookDto;
 import com.bookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto createBookRequestDto);
 
     BookDto findById(Long id);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto updateById(Long id, CreateBookRequestDto createBookRequestDto);
 
