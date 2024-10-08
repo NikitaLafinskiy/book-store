357dfa6deeaa732a31b8946bf1168a12d4ddc1de
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-10-08 18:45:50 +0200

    init

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findAllByCategoryId(Long categoryId);
 }
