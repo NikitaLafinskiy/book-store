c407738e1bd6a2d8c714e9a6d9a395f8de323c24
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-10-10 13:23:32 +0200

    made changes

--- src/main/java/com/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/bookstore/controller/AuthenticationController.java
@@ -5,7 +5,10 @@ import com.bookstore.dto.user.UserLoginResponseDto;
 import com.bookstore.dto.user.UserRegistrationRequestDto;
 import com.bookstore.dto.user.UserRegistrationResponseDto;
 import com.bookstore.exception.RegistrationException;
+import com.bookstore.service.auth.AuthenticationService;
 import com.bookstore.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
     private final UserService userService;
 
+    @Operation(summary = "Register a new user", responses = {
+            @ApiResponse(responseCode = "200", description = "User registered successfully"),
+            @ApiResponse(responseCode = "400", description = "User already exists")
+    })
     @PostMapping("/registration")
     public UserRegistrationResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto userRegistrationRequestDto)
@@ -26,9 +34,12 @@ public class AuthenticationController {
         return userService.register(userRegistrationRequestDto);
     }
 
+    @Operation(summary = "Login using the password and the username", responses = {
+            @ApiResponse(responseCode = "200", description = "User logged in successfully")
+    })
     @PostMapping("/login")
     public UserLoginResponseDto login(
             @RequestBody @Valid UserLoginRequestDto userLoginRequestDto) {
-        return userService.login(userLoginRequestDto);
+        return authenticationService.login(userLoginRequestDto);
     }
 }
--- src/main/java/com/bookstore/service/user/UserService.java
+++ src/main/java/com/bookstore/service/user/UserService.java
@@ -1,12 +1,8 @@
 package com.bookstore.service.user;
 
-import com.bookstore.dto.user.UserLoginRequestDto;
-import com.bookstore.dto.user.UserLoginResponseDto;
 import com.bookstore.dto.user.UserRegistrationRequestDto;
 import com.bookstore.dto.user.UserRegistrationResponseDto;
 
 public interface UserService {
     UserRegistrationResponseDto register(UserRegistrationRequestDto userRegistrationRequestDto);
-
-    UserLoginResponseDto login(UserLoginRequestDto userLoginRequestDto);
 }
