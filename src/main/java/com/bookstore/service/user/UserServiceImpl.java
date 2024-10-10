c6a3980785bfa237ce732dfe1e924701eb13e86d
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-10-10 12:18:26 +0200

    some changes

--- src/main/java/com/bookstore/service/user/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/user/UserServiceImpl.java
@@ -1,16 +1,21 @@
 package com.bookstore.service.user;
 
+import com.bookstore.dto.cart.ShoppingCartDto;
 import com.bookstore.dto.user.UserLoginRequestDto;
 import com.bookstore.dto.user.UserLoginResponseDto;
 import com.bookstore.dto.user.UserRegistrationRequestDto;
 import com.bookstore.dto.user.UserRegistrationResponseDto;
 import com.bookstore.entity.Role;
+import com.bookstore.entity.ShoppingCart;
 import com.bookstore.entity.User;
 import com.bookstore.exception.RegistrationException;
+import com.bookstore.mapper.ShoppingCartMapper;
 import com.bookstore.mapper.UserMapper;
 import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.ShoppingCartRepository;
 import com.bookstore.repository.UserRepository;
 import com.bookstore.service.token.JwtUtil;
+import jakarta.transaction.Transactional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,8 +34,11 @@ public class UserServiceImpl implements UserService {
     private final RoleRepository roleRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final ShoppingCartMapper shoppingCartMapper;
 
     @Override
+    @Transactional
     public UserRegistrationResponseDto register(UserRegistrationRequestDto
                                                             userRegistrationRequestDto) {
         if (userRepository.existsByEmail(userRegistrationRequestDto.getEmail())) {
@@ -39,7 +47,10 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toEntity(userRegistrationRequestDto);
         user.setPassword(passwordEncoder.encode(userRegistrationRequestDto.getPassword()));
         user.setRoles(Set.of(roleRepository.findByRoleName(DEFAULT_ROLE)));
-        return userMapper.toDto(userRepository.save(user));
+        userRepository.save(user);
+        ShoppingCart shoppingCart = new ShoppingCart(user);
+        shoppingCartRepository.save(shoppingCart);
+        return userMapper.toDto(user);
     }
 
     @Override
