5067a1c0b05486a13b0212f5a8bb060df11e9917
Nikita <niklafinskiy@gmail.com>
2024-10-14 19:47:27 +0300

    made changes

--- src/main/java/com/bookstore/controller/ShoppingCartController.java
+++ src/main/java/com/bookstore/controller/ShoppingCartController.java
@@ -10,7 +10,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,7 +35,8 @@ public class ShoppingCartController {
     @PostMapping
     @PreAuthorize("hasRole('USER')")
     public ShoppingCartDto addCartItem(@AuthenticationPrincipal String email,
-                                       @RequestBody @Valid AddBookCartRequestDto addBookCartRequestDto) {
+                                       @RequestBody @Valid AddBookCartRequestDto
+                                               addBookCartRequestDto) {
         return shoppingCartService.addCartItem(email, addBookCartRequestDto);
     }
 
